18fdf4a2f6efdacf6c6f136480bb6585be63570c
Marin <marinmiletic96@gmail.com>
2022-07-21 22:01:14 +0200

    first commit 2.0

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,44 @@
+import { createContext, useState, useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+  const { loading, error, data } = useQuery(COLLECTIONS);
+
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data]);
+
+  const value = { categoriesMap, loading };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
